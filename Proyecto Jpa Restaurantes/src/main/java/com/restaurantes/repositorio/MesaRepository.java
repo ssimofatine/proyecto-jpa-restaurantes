0e9ddfe46825b74c4a7f33d85e0c061c81ad2436
ssimofatine <simohaji147@gmail.com>
2026-04-28 18:06:50 +0200

    edit

--- Proyecto Jpa Restaurantes/src/main/java/com/restaurantes/repositorio/MesaRepository.java
+++ Proyecto Jpa Restaurantes/src/main/java/com/restaurantes/repositorio/MesaRepository.java
@@ -1,4 +1,14 @@
 package com.restaurantes.repositorio;
+import com.restaurantes.modelo.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class MesaRepository {
 }
